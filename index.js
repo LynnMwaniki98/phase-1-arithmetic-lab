4792452b9779650e6971a247750feacb708e05ec
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-15 23:32:22 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31, num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const max = Math.max(3, 15, 20);
+
+const num3 = 24, num4 = 5;
+const mod = num3 % num4
+
+const random = Math.floor(Math.random() *11 );
